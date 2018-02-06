e4a35f049df1bdcf9dd1a2b1fe36df40c12c0356
Nathan Carlson <ncarlson@iteris.com>
2018-02-06 14:05:08 -0600

    Update HW2

--- HW2/src/Multi.java
+++ HW2/src/Multi.java
@@ -1,7 +1,8 @@
 package HW2;
 
 public class Multi extends Thread{
-
+    private static final double V1 = 1.0;
+    private static final double V2 = 2.0;
     double[][] M1;
     double[][] M2;
     double[][] M3;
@@ -15,10 +16,6 @@ public class Multi extends Thread{
         M1 = _M1;
         M2 = _M2;
         M3 = _M3;
-        FillN(M1, 1.0);
-        FillN(M2, 2.0);
-        FillZero(M3);
-        
     }
    
     // Multiply 
@@ -40,36 +37,9 @@ public class Multi extends Thread{
         System.out.println(String.format("Id: %2d [%2d, %2d)", id, start, stop));
         for (int i = start; i < stop; i++) {
             for (int j = 0; j < N; j++) {
-                double sum = 0;
                 for (int k = 0; k < N; k++) {
-                    sum += M1[i][k] * M2[k][j];
+                    M3[i][j] += M1[i][k] * M2[k][j];
                 }
-                M3[i][j] = sum;
-            }
-        }
-    }
-    
-    private void FillRandom(double[][] M){
-        int N = M.length;;
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<N; j++){
-                M[i][j] = Math.random();
-            }
-        }
-    }
-    private void FillN(double[][] M, double _n){
-        int N = M.length;;
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<N; j++){
-                M[i][j] = _n;
-            }
-        }
-    }
-    private void FillZero(double[][] M){
-        int N = M.length;;
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<N; j++){
-                M[i][j] = 0.0;
             }
         }
     }
@@ -78,28 +48,34 @@ public class Multi extends Thread{
         int N = Integer.parseInt(args[0]);
         int N_thread = Integer.parseInt(args[1]);
         
-        double[][] M1 = new double[N][N];
-        double[][] M2 = new double[N][N];
-        double[][] M3 = new double[N][N];
+        double[][] _M1 = new double[N][N];
+        double[][] _M2 = new double[N][N];
+        double[][] _M3 = new double[N][N];
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                _M1[i][j] = V1;
+                _M2[i][j] = V2;
+                _M3[i][j] = 0.0;
+            }
+        }
+
         Multi[] threads = new Multi[N_thread];
         for( int i = 0; i<N_thread; i++){
-            threads[i] = new Multi(M1, M2, M3, N, N_thread, i);
+            threads[i] = new Multi(_M1, _M2, _M3, N, N_thread, i);
             threads[i].start();
         }
-        
+
         for( int i = 0; i<N_thread; i++){
             try{
                 threads[i].join();
-            }            
-                
-            catch(InterruptedException e){              
             }
+            catch(InterruptedException e){}
         }
-        //System.out.println(M3[0][0]);
+
         int n_incorrect = 0;
         for(int i = 0; i<N; i++){
             for(int j = 0; j<N; j++){
-                if(M3[i][j]!=60.0){
+                if(_M3[i][j]!= N * V1 * V2){
                     n_incorrect++;
                 }
             }
