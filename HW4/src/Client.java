f9238a933e9360a877c515927764fb8e86c46463
Nathan Carlson <nathan.l.carlson@und.edu>
2018-03-11 19:13:54 -0500

    HW4 Update

--- HW4/src/Client.java
+++ HW4/src/Client.java
@@ -10,8 +10,19 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Client {
     public static void main(String args[]){
         String host = "127.0.0.1";
-        String name = "ncarlson";
-        int port = 8080;
+        if(args.length != 2){
+            System.out.println("Usage: [port] [userId]");
+            return;
+        }
+        String name = args[1];
+        int port;
+        try{
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e){
+            System.out.println("Usage: [port] [userId]");
+            System.out.println("Port must be an integer");
+            return;
+        }
         try {
             Registry registry = LocateRegistry.getRegistry(host, port);
             ClientManager server = (ClientManager)registry.lookup("ClientManager");
@@ -22,15 +33,11 @@ public class Client {
             catch(RemoteException e){
                 jobs = server.connect(name);
             }
-
-            // for(int i=0; i<15; ++i){
-            //     int j = ThreadLocalRandom.current().nextInt(0, jobs.size());
-            //     Worker worker = server.requestWork(name, jobs.get(j));
-            //     worker.doWork();
-            //     server.submitResults(name, worker);
-            //     System.out.println("My Score: "+server.getScore(name));
-            // }
-
+            int j = ThreadLocalRandom.current().nextInt(0, jobs.size());
+            Worker worker = server.requestWork(name, jobs.get(j));
+            worker.doWork();
+            server.submitResults(name, worker);
+            System.out.println(name+" Score: "+server.getScore(name));
         }
         catch (Exception re) {
             re.printStackTrace();
--- HW4/src/Server.java
+++ HW4/src/Server.java
@@ -7,8 +7,6 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,24 +17,14 @@ import java.sql.Statement;
 
 public class Server implements ClientManager {
 
-    private List<String> names;
     private List<String> jobs;
     private Connection conn;
-    private Map<String, Float> scores;
-    private Map<Integer, String> tasks;
-    private Map<Integer, List<Integer>> params;
-    private Map<Integer, List<Integer>> answers;
     private Connection db;
-    public Server() {
+    public Server() throws RemoteException {
         jobs = new ArrayList<String>();
         jobs.add("PrimeChecker");
         jobs.add("FractionReducer");
         jobs.add("NumberSorter");
-        names = new ArrayList<String>();
-        scores = new HashMap<String, Float>();
-        tasks = new HashMap<Integer, String>();
-        params = new HashMap<Integer, List<Integer>>();
-        answers = new HashMap<Integer, List<Integer>>();
         String url = "jdbc:mysql://localhost:3306/hw4";
 		String userId = "hw4admin";
 		String passwd = "hw4pass";
@@ -45,6 +33,7 @@ public class Server implements ClientManager {
         }
         catch(SQLException e){
             e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
         }
     }
 
@@ -54,15 +43,17 @@ public class Server implements ClientManager {
             throw new RemoteException("User Id already exists");
         }
         else{
-            String sql = "insert into `users` (userId, score) values ('"+userid+"', 0);";
-            executeUpdate(sql);
-            // System.out.println("Scores: ");
-            // for (String name: scores.keySet()){
-            //     String key = name.toString();
-            //     String value = scores.get(name).toString();
-            //     System.out.println(key + " " + value);
-            // }
-            // System.out.println();
+            try{
+                String sql = "INSERT INTO `users` (userId, score) VALUES ('"+userid+"', 0);";
+                Statement stmt = db.createStatement();
+                int rs = stmt.executeUpdate(sql);
+                stmt.close();
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RemoteException("500 Error Something went wrong");
+            }
+            printScores();
             return jobs;
         }
     }
@@ -71,7 +62,27 @@ public class Server implements ClientManager {
             throw new RemoteException("User Id does not exist");
         }
         else{
+            printScores();
             return jobs;
+        }
+    }
+    private void printScores() throws RemoteException{
+        // Print all scores
+        try{
+            String sql = "SELECT * FROM `users`;";
+            Statement stmt = db.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Scores: ");
+            while(rs.next()){
+                System.out.println(rs.getString("userId")+" "+rs.getInt("score"));
+            }
+            System.out.println();
+            rs.close();
+            stmt.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
         }
     }
     public Worker requestWork(String userid, String taskName) throws RemoteException{
@@ -82,35 +93,37 @@ public class Server implements ClientManager {
         Worker worker;
         int cur_task;
         // Store params
-        ArrayList<Integer> t_params = new ArrayList();
         if(taskName.equals("PrimeChecker")){
             int rndInt = ThreadLocalRandom.current().nextInt(0, 1 << 10);
-            t_params.add(rndInt);
+            cur_task = insertTask("PrimeChecker", rndInt, null, null);
             worker = new PrimeChecker(cur_task, rndInt);
         }
         else if(taskName.equals("FractionReducer")){
             int rndInt1 = ThreadLocalRandom.current().nextInt(0, 1 << 10);
             int rndInt2 = ThreadLocalRandom.current().nextInt(0, 1 << 10);
-            t_params.add(rndInt1);
-            t_params.add(rndInt2);
+            cur_task = insertTask("FractionReducer", rndInt1, rndInt2, null);
             worker = new FractionReducer(cur_task, rndInt1, rndInt2);
         }
         else if(taskName.equals("NumberSorter")){
             int rndInt1 = ThreadLocalRandom.current().nextInt(0, 1 << 10);
             int rndInt2 = ThreadLocalRandom.current().nextInt(0, 1 << 10);
             int rndInt3 = ThreadLocalRandom.current().nextInt(0, 1 << 10);
-            t_params.add(rndInt1);
-            t_params.add(rndInt2);
-            t_params.add(rndInt3);;
+            cur_task = insertTask("NumberSorter", rndInt1, rndInt2, rndInt3);
             worker = new NumberSorter(cur_task, rndInt1, rndInt2, rndInt3);
         }
         else{
             throw new RemoteException("Task does not exist");
         }
-        // Save task
-        params.put(cur_task, t_params);
-        tasks.put(cur_task, taskName);
-        cur_task++;
+        try{
+            String sql = "INSERT into `usertasks` (userId, taskId) VALUES ('"+userid+"', "+cur_task+"); ";
+            Statement stmt = db.createStatement();
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
         return worker;
     }
     public void submitResults(String userid, Worker answer) throws RemoteException{
@@ -120,76 +133,150 @@ public class Server implements ClientManager {
         }
         // Get task Id
         int taskId = answer.getTaskId();
-        // Check if valid task Id
-        if(!(tasks.containsKey(taskId))){
-            throw new RemoteException("Task Id does not exist");
-        }
-        // Get type of task
-        String taskName = tasks.get(taskId);
-        // Store answers
-        ArrayList<Integer> t_answers = new ArrayList();
-        if(taskName.equals("PrimeChecker")){
-            PrimeChecker t_answer = (PrimeChecker)answer;
-            t_answers.add(t_answer.isPrime() ? 1 : 0);
-        }
-        else if(taskName.equals("FractionReducer")){
-            FractionReducer t_answer = (FractionReducer)answer;
-            t_answers.add(t_answer.getNumerator());
-            t_answers.add(t_answer.getDenominator());;
-        }
-        else if(taskName.equals("NumberSorter")){
-            NumberSorter t_answer = (NumberSorter)answer;
-            t_answers.add(t_answer.getSmallest());
-            t_answers.add(t_answer.getMiddle());
-            t_answers.add(t_answer.getLargest());;
-        }
-        else{
-            throw new RemoteException("Task does not exist");
-        }
-        answers.put(taskId, t_answers);
-        System.out.println("Task: " + taskName);
-        for (Integer tId: tasks.keySet()){
-            String task = tasks.get(tId).toString();
-            if(task.equals(taskName)){
-                System.out.println("Params: " + params.get(tId));
-                System.out.println("Answer: " + answers.get(tId) + '\n');
-            }
-        }
 
-        scores.put(userid, scores.get(userid)+1.F);
+        // Update row in database
+        String taskName;
+        try{
+            String sql = "SELECT * FROM `tasks` WHERE taskId='"+taskId+"';";
+            Statement stmt = db.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery(sql);
+            if(!rs.next()){
+                throw new RemoteException("Task Id does not exist");
+            }
+            // Get type of task
+            taskName  = rs.getString("taskName");
+            // Store answers
+            if(taskName.equals("PrimeChecker")){
+                PrimeChecker t_answer = (PrimeChecker)answer;
+                rs.updateInt("out1", t_answer.isPrime() ? 1 : 0);
+            }
+            else if(taskName.equals("FractionReducer")){
+                FractionReducer t_answer = (FractionReducer)answer;
+                rs.updateInt("out1", t_answer.getNumerator());
+                rs.updateInt("out2", t_answer.getDenominator());
+            }
+            else if(taskName.equals("NumberSorter")){
+                NumberSorter t_answer = (NumberSorter)answer;
+                rs.updateInt("out1", t_answer.getSmallest());
+                rs.updateInt("out2", t_answer.getMiddle());
+                rs.updateInt("out3", t_answer.getLargest());
+            }
+            else{
+                throw new RemoteException("Task does not exist");
+            }
+            rs.updateRow();
+            rs.close();
+            stmt.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
+        // Print all occurences of that task
+        try{
+            String sql = "SELECT * FROM `tasks` WHERE taskName='"+taskName+"';";
+            Statement stmt = db.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Task: "+taskName);
+            while(rs.next()){
+                System.out.println("Params: " + rs.getInt("in1") +" "+ rs.getInt("in2") +" "+ rs.getInt("in3"));
+                System.out.println("Answer: " + rs.getInt("out1") +" "+ rs.getInt("out2") +" "+ rs.getInt("out3"));
+            }
+            System.out.println();
+            rs.close();
+            stmt.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
+        // Update score
+        try{
+            String sql = "UPDATE `users` SET score = score + 1 WHERE userId='"+userid+"';";
+            Statement stmt = db.createStatement();
+            int rs = stmt.executeUpdate(sql);
+            stmt.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
     }
     public float getScore(String userid) throws RemoteException{
         if(!userExists(userid)){
             throw new RemoteException("User Id does not exist");
         }
+        try{
+            String sql = "SELECT score FROM `users` WHERE userId='"+userid+"';";
+            Statement stmt = db.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            rs.next();
+            int score = rs.getInt("score");
+            rs.close();
+            stmt.close();
+            return score;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
+    }
 
-        return scores.get(userid);
+    private int insertTask(String name, Integer a, Integer b, Integer c) throws RemoteException{
+        String fields = " (taskName, in1";
+        String values = " VALUES ('"+name+"', "+a;
+        if(b != null){
+            fields += ", in2";
+            values += ", "+b;
+        }
+        if(c != null){
+            fields += ", in3";
+            values += ", "+c;
+        }
+        fields += ") ";
+        values += "); ";
+        String sql = "INSERT INTO `tasks` "+fields+values;
+        try{
+            Statement stmt = db.createStatement();
+            stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            int auto_id = rs.getInt(1);
+            rs.close();
+            stmt.close();
+            return auto_id;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
+    }
+    private boolean userExists(String userId) throws RemoteException{
+        String sql = "SELECT * FROM `users` WHERE userId='"+userId+"';";
+        try{
+            Statement stmt = db.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            boolean exists = rs.next();
+            rs.close();
+            stmt.close();
+            return exists;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RemoteException("500 Error Something went wrong");
+        }
     }
     private int executeUpdate(String sql){
         try{
             Statement stmt = db.createStatement();
             int rs = stmt.executeUpdate(sql);
+            stmt.close();
             return rs;
         }
         catch(SQLException e){
             e.printStackTrace();
+            System.exit(1);
             return 0;
-        }
-    }
-    private int insertTask(String name, Integer a, Integer b, Integer c){
-        String sql = "INSERT INTO `tasks` (taskName, in1, in2, in3) VALUES ("+name+","+a+","+b+","+c+");"
-    }
-    private boolean userExists(String userId){
-        String sql = "SELECT * FROM `users` WHERE userId='"+userId+"';";
-        System.out.println(sql);
-        try{
-            Statement stmt = db.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-            return rs.next();
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-            return false;
         }
     }
     public void createTables(){
@@ -234,7 +321,7 @@ public class Server implements ClientManager {
 
         if(!(args.length == 1 || args.length == 2)){
             System.out.println("Usage: [port]");
-            System.out.println("or, to reset the database, \nUsage: [port] reset");
+            System.out.println("or, to reset/initialize the database, \nUsage: [port] reset");
             return;
         }
         if(args.length == 2 && !args[1].equals("reset")){
